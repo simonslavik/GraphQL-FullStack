b92b2328e3d4ca46265db9264c54a0f8fbf3b03c
simonslavik <simonslavik007@gmail.com>
2025-09-16 14:16:44 +0200

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './compontents/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
 import Project from './pages/Project';
