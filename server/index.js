c2942cf2e02b910e54b86161bc03d241f2dda664
simonslavik <simonslavik007@gmail.com>
2025-09-16 09:40:38 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
